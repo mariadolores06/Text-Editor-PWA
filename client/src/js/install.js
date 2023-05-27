2da458970a1e45f646a377d0ee3fb4b48bd42612
unknown <maria.rodriguez@northwestern.edu>
2023-05-27 11:25:52 -0500

    install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// triggered when the app runs and shows the install button 
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  console.log("beforeinstallprompt event triggered!");
+  window.deferredPrompt = event;
+  butInstall.style.display = "inline-block";
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// once install button is clicked- user chooses whether ot not to continue installing the app. Automatically installs if they choose yes
+butInstall.addEventListener('click', async () => {
+  console.log("Install button clicked!")
+  window.deferredPrompt.prompt();
+  const { outcome } = await window.deferredPrompt.userChoice;
+  console.log("User's choice to install application?", outcome);
+  if (outcome === 'accepted') {
+    window.deferredPrompt = null;
+  }
+});
+
+//Event listener "appInstalled" -triggered once the app has finishd installing. Hides the button afterwards
+window.addEventListener('appinstalled', (event) => {
+  console.log("Just Another Text Editor has been successfully installed!")
+  butInstall.style.display = "none";
+  window.deferredPrompt = null;
+});
